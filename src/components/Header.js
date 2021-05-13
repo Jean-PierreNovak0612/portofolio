7e836cdc87f99ba9213137942a42a8d7c298d1f4
Jean-Pierre Novak <jeanpierrenovak20@gmail.com>
2021-05-13 19:40:30 +0200

    Modified the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,14 +7,14 @@ const Header = ({ links = [] }) => {
 
     const renderSublinks = (sublinks) => {
         return sublinks.map(sublink => {
-            return <li key={sublink.title}><Link className="nav-link" to="/">{sublink.title}</Link></li>
+            return <li key={sublink.title}><Link className="nav-link" to={sublink.location}>{sublink.title}</Link></li>
         })
     }
 
     const renderLinkWithSublinks = (link) => {
         return (
             <li key={link.title}>
-                <Link className="nav-link desktop-link" to="/">{link.title}</Link>
+                <Link className="nav-link desktop-link" to={link.location}>{link.title}</Link>
                 <label htmlFor={`sublinks-${link.position}`} className="nav-link">{link.title} +</label>
                 <input type="checkbox" id={`sublinks-${link.position}`} className="sublinks-toggle" />   
                 <ul className="sublinks">
@@ -29,13 +29,13 @@ const Header = ({ links = [] }) => {
         if(!links) return null;
         return links.map(link => {
             if(link.sublinks) return renderLinkWithSublinks(link);
-            return <li key={link.title}><Link className="nav-link" to="/">{link.title}</Link></li>
+            return <li key={link.title}><Link className="nav-link" to={link.location}>{link.title}</Link></li>
         })
     }
 
     return (
         <header>
-            <h1 className="logo">MyPortoPage</h1>
+            <Link to="/" className="logo"><h1>MyPortoPage</h1></Link>
             <input type="checkbox" id="toggle-nav" className="toggle-navbar" />
             <label htmlFor="toggle-nav" className="toggle-nav-label"><span className="hamburger-icon"></span><span className="close-nav-icon"></span></label>
             <nav>
