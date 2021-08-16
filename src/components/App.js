b2f4f2b62c71a0a0041b50960430beb5ad9c2e47
Jean-Pierre Novak <jeanpierrenovak20@gmail.com>
2021-08-16 14:10:55 +0200

    Deleted old portofolio page. Creating new one

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Header from './Header';
-import HomePage from './HomePage';
-import ContactPage from './ContactPage';
-import ProjectList from './ProjectList';
-import AboutPage from './AboutPage';
-import { NAV_LINKS, LIST_ITEMS, ARTICLE_LIST } from '../data';
-
-import '../css/style.css';
+import Header from './Header'
 
 const App = () => {
     return (
         <div>
-            <Router basename={process.env.PUBLIC_URL}>
-                <Header links={NAV_LINKS} />
-                <Switch>
-                    <Route path="/" exact component={HomePage} />
-                    <Route path="/contact" exact component={ContactPage} />
-                    <Route path="/projects" exact component={() => <ProjectList listItems={LIST_ITEMS} />} />
-                    <Route path="/about" exact component={() => <AboutPage contents={ARTICLE_LIST} />} />
-                </Switch>
-            </Router>
+            <Header />
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import '../css/Header.css';
-
-const Header = ({ links = [] }) => {
-
-    const renderSublinks = (sublinks) => {
-        
-        return sublinks.map(sublink => {
-            if(sublink.location.substring(0, 4) === "http") return <li key={sublink.title}><a className="nav-link" target="_blank" rel="noreferrer" href={sublink.location}>{sublink.title}</a></li>
-            return <li key={sublink.title}><Link className="nav-link" to={sublink.location}>{sublink.title}</Link></li>
-        })
-    }
-
-    const renderLinkWithSublinks = (link) => {
-        return (
-            <li key={link.title} className="nav-parent-link">
-                <Link className="nav-link desktop-link" to={link.location}>{link.title}</Link>
-                <label htmlFor={`sublinks-${link.position}`} className="nav-link">{link.title} +</label>
-                <input type="checkbox" id={`sublinks-${link.position}`} className="sublinks-toggle" />   
-                <ul className="sublinks">
-                    {renderSublinks(link.sublinks)}
-                </ul>     
-            </li>
-        )
-    }
-
-    const renderLinks = () => {
-        if(!links) return null;
-        return links.map(link => {
-            if(link.sublinks) return renderLinkWithSublinks(link);
-            return <li key={link.title}><Link className="nav-link" to={link.location}>{link.title}</Link></li>
-        })
-    }
-
+const Header = () => {
     return (
-        <header>
-            <Link to="/" className="logo"><h1>MyPortoPage</h1></Link>
-            <input type="checkbox" id="toggle-nav" className="toggle-navbar" />
-            <label htmlFor="toggle-nav" className="toggle-nav-label"><span className="hamburger-icon"></span><span className="close-nav-icon"></span></label>
-            <nav>
-                <ul>
-                    {renderLinks()}
-                </ul>
-            </nav>
-        </header>
+        <div>Header</div>
     )
 }
 
