73135500a347e8e58c577c98d5f33d7e0de03d13
Jean-Pierre Novak <jeanpierrenovak20@gmail.com>
2021-08-18 16:42:39 +0200

    Navigation for large displays completed

--- src/components/NavbarLink.js
+++ src/components/NavbarLink.js
@@ -5,7 +5,7 @@ import '../css/NavbarLink.css';
 
 // The NavbarLink function is used to create the navigation links
 
-const NavbarLink = ({ link, closeNav, navigation }) => {
+const NavbarLink = ({ link, closeNav, navigation, addedClass = "" }) => {
 
     // Function that closes the navbar when a link has been clicked
 
@@ -14,7 +14,7 @@ const NavbarLink = ({ link, closeNav, navigation }) => {
     }
 
     return (
-        <li className="navbar-link-container">
+        <li className={"navbar-link-container " + addedClass}>
             <Link className={navigation ? 'navbar-link active' : 'navbar-link'} to={link.location} onClick={Selected}>{link.title}</Link>
         </li>
     )
--- src/components/NavbarSublink.js
+++ src/components/NavbarSublink.js
@@ -14,14 +14,23 @@ const NavbarSublink = ({ link, closeNav, navigation }) => {
     
     const GenerateLinks = () => {
         return link.sublinks.map(sublink => {
-            return <NavbarLink className="navbar-sublink" key={sublink.title} closeNav={closeNav} link={sublink} navigation={navigation} />;
+            return <NavbarLink className="navbar-sublink" key={sublink.title} closeNav={closeNav} link={sublink} navigation={navigation} addedClass="sublink" />;
         })
+    }
+
+    // Function that determines the class that the sublink container should have
+
+    const SublinkListClass = () => {
+        if(navigation && toggleDropdown) return 'navbar-sublink-container active scrolled'
+        if(toggleDropdown) return 'navbar-sublink-container active'
+        if(navigation) return 'navbar-sublink-container scrolled'
+        return 'navbar-sublink-container'
     }
 
     return (
         <li className="navbar-link-container">
-            <label className="navbar-label" onClick={() => setToggleDropdown(!toggleDropdown)}>{link.title} <span className={toggleDropdown ? 'dropdown-icon active' : 'dropdown-icon'}>+</span></label>
-            <ul className={toggleDropdown ? 'navbar-sublink-container active' : 'navbar-sublink-container'}>
+            <label className={navigation ? 'navbar-label active' : 'navbar-label'} onClick={() => setToggleDropdown(!toggleDropdown)}>{link.title} <span className={toggleDropdown ? 'dropdown-icon active' : 'dropdown-icon'}>+</span></label>
+            <ul className={SublinkListClass()}>
                 {GenerateLinks()}
             </ul>
         </li>
